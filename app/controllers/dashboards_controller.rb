f11e24b6efd56c151fbfd42c2d9a5b2f93de85b2
Alex <awheeler@bu.edu>
2014-03-09 23:43:50 -0400

    Alex's initial commit and corrects camelcase in dashboard controller

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -26,18 +26,18 @@ class DashboardsController < ApplicationController
 
 		json = JSON.parse(token.get('http://api.fitbit.com/1/user/-/activities/date/' + cur_date+ '.json').body)
 		
-		stepGoal=json['goals']['steps']
+		step_goal=json['goals']['steps']
 		cur_steps=json['summary']['steps']
 
 		
 		#This data could be super cool and isn't offered on Fitbit dashbaord. (besides very active mins)
-		cur_fairlyActive= json['summary']['fairlyActiveMinutes']
-		cur_lightlyActive= json['summary']['lightlyActiveMinutes']
-		cur_veryActive= json['summary']['veryActiveMinutes']
-		cur_sedentaryMins= json['summary']['sedentaryMinutes']
+		cur_fairly_active= json['summary']['fairlyActiveMinutes']
+		cur_lightly_active= json['summary']['lightlyActiveMinutes']
+		cur_very_active= json['summary']['veryActiveMinutes']
+		cur_sedentary_mins= json['summary']['sedentaryMinutes']
 
 		#we should check if greater than 1
-		@stepsRemain= stepGoal-cur_steps
+		@steps_remain= step_goal-cur_steps
 		@steps = cur_steps
 
 
